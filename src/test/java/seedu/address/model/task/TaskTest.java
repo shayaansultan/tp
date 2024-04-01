6e6b7bcb38c755c9174b60a8a5f88380ff03d706
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-04-01 19:44:00 +0800

    Update TaskTest class for better code coverage

--- src/test/java/seedu/address/model/task/TaskTest.java
+++ src/test/java/seedu/address/model/task/TaskTest.java
@@ -1,12 +1,12 @@
 package seedu.address.model.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
-
     @Test
     public void isSameTask() {
         Task task1 = new Task(new Description("Buy milk"));
@@ -21,5 +21,35 @@ public class TaskTest {
 
         // different values -> returns false
         assertFalse(task1.isSameTask(task3));
+    }
+
+    @Test
+    public void testToString() {
+        Task task = new Task(new Description("Buy milk"));
+        assertEquals("[_] Buy milk", task.toString());
+
+        task.mark();
+        assertEquals("[X] Buy milk", task.toString());
+    }
+
+    @Test
+    public void testIsValidTaskString() {
+        assertTrue(Task.isValidTaskString("[_] Buy milk"));
+        assertTrue(Task.isValidTaskString("[X] Buy milk"));
+        assertFalse(Task.isValidTaskString("[_]Buy milk"));
+        assertFalse(Task.isValidTaskString("[X]Buy milk"));
+        assertFalse(Task.isValidTaskString("[ ] Buy milk"));
+        assertFalse(Task.isValidTaskString("Buy milk"));
+    }
+
+    @Test
+    public void testParseTaskString() {
+        Task task = Task.parseTaskString("[_] Buy milk");
+        assertEquals(new Task(new Description("Buy milk")), task);
+
+        task = Task.parseTaskString("[X] Buy milk");
+        Task doneTask = new Task(new Description("Buy milk"));
+        doneTask.mark();
+        assertEquals(doneTask, task);
     }
 }
